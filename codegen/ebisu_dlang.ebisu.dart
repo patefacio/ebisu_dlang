8b6820eaf6e971f9347dd1838e566f9342c97465
Daniel Davidson <dbdavidson@yahoo.com>
2013-09-23 12:07:31 -0500

    updated .gitignore to ignore pubspec.lock added readme

--- codegen/ebisu_dlang.ebisu.dart
+++ codegen/ebisu_dlang.ebisu.dart
@@ -92,6 +92,25 @@ void main() {
 
   System ebisu = system('ebisu_dlang')
     ..rootPath = '$_topDir'
+    ..introduction = '''
+Code generation library that supports generating the structure of D code
+'''
+    ..purpose = '''
+
+D is complex. The idea of this library is to put a straightjacket on developers, but give them a key to get out. Go sells itself on simplicity and it has it from a language perspective since its feature list is rather sparse. However, some have pointed out that that simplicity just hides the fact that lots of the problems that need to be solved are complex and taking away language features may not be the best way to provide simplicity.
+
+Another way is to find a set of approaches/patterns/idioms, etc that are known to work and stick with them. When the need for complexity arises, use more of the language.
+
+The intended use of this library is to generate D source that, for better or worse, is extremely consistent. It will ensure:
+
+- All names are consistently defined and cased
+- All files are put in standard places
+- All classes are laid out in standard fashion
+- All members that have accessors do it in a consistent manner
+
+Plus it will provide a key to use the language as developer sees fit. This key is in the form of allowing custom code to be comingled with generated code.
+
+'''
     ..pubSpec = (pubspec('ebisu_dlang')
         ..doc = 'A library that supports code generation of dart and others'
         ..dependencies = [
